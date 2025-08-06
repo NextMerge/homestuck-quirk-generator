cbf13691f60a29293c3fbe036653ea1787f8f624
Mark Jarjour <mj_git_signing@sent.com>
2025-08-06 18:30:57 -0400

    lower limits

--- convex/limits.ts
+++ convex/limits.ts
@@ -1,4 +1,4 @@
 export const NAME_MAX_LENGTH = 50;
 export const COLLECTION_COUNT_MAX = 15;
-export const QUIRK_COUNT_MAX = 30;
-export const ATTRIBUTE_COUNT_MAX = 50;
+export const QUIRK_COUNT_MAX = 25;
+export const ATTRIBUTE_COUNT_MAX = 20;
