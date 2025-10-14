ef7e425c59987cd41f41097d916766f9a9f5e5ef
Mark Jarjour <mj_git_signing@sent.com>
2025-10-14 17:59:27 -0400

    Up attribute limit since some canon quirks have high attribute counts

--- convex/limits.ts
+++ convex/limits.ts
@@ -1,4 +1,4 @@
 export const NAME_MAX_LENGTH = 50;
 export const COLLECTION_COUNT_MAX = 15;
 export const QUIRK_COUNT_MAX = 25;
-export const ATTRIBUTE_COUNT_MAX = 20;
+export const ATTRIBUTE_COUNT_MAX = 100;
